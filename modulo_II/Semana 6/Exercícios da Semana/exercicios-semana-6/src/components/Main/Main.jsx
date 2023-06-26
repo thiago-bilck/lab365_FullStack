94da9482d14ffc5881975c7ab0c802e5ee1aed79
thiago-bilck <thiagobilck@gmail.com>
2023-06-26 17:22:07 -0300

    feat: create ex 5 from week 6

--- modulo_II/Semana 6/Exercícios da Semana/exercicios-semana-6/src/components/Main/Main.jsx
+++ modulo_II/Semana 6/Exercícios da Semana/exercicios-semana-6/src/components/Main/Main.jsx
@@ -3,37 +3,31 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './Main.css';
+import ProductCard from "../ProductCard/ProductCard";
 
 const Main = () => {
     
 
     return(
 
-        <div className="card-group">
-  <div className="card">
-    <img src="..." className="card-img-top" alt="..."/>
-    <div className="card-body">
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-  <div className="card">
-    <img src="..." className="card-img-top" alt="..."/>
-    <div className="card-body">
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-      <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-  <div className="card">
-    <img src="..." className="card-img-top" alt="..."/>
-    <div className="card-body">
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-      <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
+        <div className="row">
+         <div className="col-lg-4 col-md-12 mt-3 "> 
+          <ProductCard name={'Mochila Aventureira Pro'} description={'A Mochila Aventureira Pro é o companheiro perfeito para suas aventuras ao ar livre. Com design durável e funcional, ela oferece amplo espaço de armazenamento e recursos inteligentes para atender às necessidades dos entusiastas de atividades ao ar livre.'} features={['Com a capacidade expansível da Mochila Aventureira Pro, você terá espaço de sobra para guardar todos os seus equipamentos essenciais. Seja para uma caminhada de um dia ou uma expedição mais longa, esta mochila se adapta às suas necessidades.', 'Não importa as condições climáticas, a Mochila Aventureira Pro foi projetada para enfrentar todos os desafios. Seu material resistente à água e durável protege seus pertences de chuva, neve e umidade, mantendo-os secos e seguros.', 'Com alças ajustáveis e acolchoadas, a Mochila Aventureira Pro oferece conforto excepcional durante longas caminhadas ou trilhas. Além disso, o sistema de ventilação nas costas proporciona circulação de ar adequada, mantendo você fresco e confortável mesmo em atividades intensas.']} price={899} imageUrl={'https://i.imgur.com/WSvGRAW.png'} id={1}></ProductCard>
+          </div>
+
+          <div className="col-lg-4 col-md-12 mt-3">
+          <ProductCard name={'Mochila Aventureira Pro'} description={'A Mochila Aventureira Pro é o companheiro perfeito para suas aventuras ao ar livre. Com design durável e funcional, ela oferece amplo espaço de armazenamento e recursos inteligentes para atender às necessidades dos entusiastas de atividades ao ar livre.'} features={['Com a capacidade expansível da Mochila Aventureira Pro, você terá espaço de sobra para guardar todos os seus equipamentos essenciais. Seja para uma caminhada de um dia ou uma expedição mais longa, esta mochila se adapta às suas necessidades.', 'Não importa as condições climáticas, a Mochila Aventureira Pro foi projetada para enfrentar todos os desafios. Seu material resistente à água e durável protege seus pertences de chuva, neve e umidade, mantendo-os secos e seguros.', 'Com alças ajustáveis e acolchoadas, a Mochila Aventureira Pro oferece conforto excepcional durante longas caminhadas ou trilhas. Além disso, o sistema de ventilação nas costas proporciona circulação de ar adequada, mantendo você fresco e confortável mesmo em atividades intensas.']} price={899} imageUrl={'https://i.imgur.com/WSvGRAW.png'} id={1}></ProductCard>
+          </div>
+
+          <div className="col-lg-4 col-md-12 mt-3">
+          <ProductCard name={'Mochila Aventureira Pro'} description={'A Mochila Aventureira Pro é o companheiro perfeito para suas aventuras ao ar livre. Com design durável e funcional, ela oferece amplo espaço de armazenamento e recursos inteligentes para atender às necessidades dos entusiastas de atividades ao ar livre.'} features={['Com a capacidade expansível da Mochila Aventureira Pro, você terá espaço de sobra para guardar todos os seus equipamentos essenciais. Seja para uma caminhada de um dia ou uma expedição mais longa, esta mochila se adapta às suas necessidades.', 'Não importa as condições climáticas, a Mochila Aventureira Pro foi projetada para enfrentar todos os desafios. Seu material resistente à água e durável protege seus pertences de chuva, neve e umidade, mantendo-os secos e seguros.', 'Com alças ajustáveis e acolchoadas, a Mochila Aventureira Pro oferece conforto excepcional durante longas caminhadas ou trilhas. Além disso, o sistema de ventilação nas costas proporciona circulação de ar adequada, mantendo você fresco e confortável mesmo em atividades intensas.']} price={899} imageUrl={'https://i.imgur.com/WSvGRAW.png'} id={1}></ProductCard>
+          </div>
+
+          <div className="col-lg-4 col-md-12 mt-3">
+          <ProductCard name={'Mochila Aventureira Pro'} description={'A Mochila Aventureira Pro é o companheiro perfeito para suas aventuras ao ar livre. Com design durável e funcional, ela oferece amplo espaço de armazenamento e recursos inteligentes para atender às necessidades dos entusiastas de atividades ao ar livre.'} features={['Com a capacidade expansível da Mochila Aventureira Pro, você terá espaço de sobra para guardar todos os seus equipamentos essenciais. Seja para uma caminhada de um dia ou uma expedição mais longa, esta mochila se adapta às suas necessidades.', 'Não importa as condições climáticas, a Mochila Aventureira Pro foi projetada para enfrentar todos os desafios. Seu material resistente à água e durável protege seus pertences de chuva, neve e umidade, mantendo-os secos e seguros.', 'Com alças ajustáveis e acolchoadas, a Mochila Aventureira Pro oferece conforto excepcional durante longas caminhadas ou trilhas. Além disso, o sistema de ventilação nas costas proporciona circulação de ar adequada, mantendo você fresco e confortável mesmo em atividades intensas.']} price={899} imageUrl={'https://i.imgur.com/WSvGRAW.png'} id={1}></ProductCard>
+          </div>
+          
+
 </div>
     )
 }
---
+++ modulo_II/Semana 6/Exercícios da Semana/exercicios-semana-6/src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,31 @@
+import PropTypes from "prop-types";
+import "./ProductCard.css"
+
+function ProductCard({ name, price, description, features, imageUrl, id }) {
+  return (
+    <div className="card" key={id}>
+      <img src={imageUrl} className="card-img-top"  alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">{name}</h5>
+        <p className="card-text">{description}</p>
+        <ul className="card-text">
+            {features.map((f, i) => {
+                <li key={i}>{f}</li>                
+            }) }
+        </ul>
+        <p className="card-text"><small className="text-body-secondary">R$: {price}</small></p>
+      </div>
+    </div>
+  );
+}
+
+ProductCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  description: PropTypes.string.isRequired,
+  features: PropTypes.arrayOf(PropTypes.string).isRequired,
+  imageUrl: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+};
+
+export default ProductCard;
