59c4e86824ea60d181754ada16e6f43f656b2c31
thiago-bilck <thiagobilck@gmail.com>
2023-06-29 12:10:56 -0300

    feat: create ex 6 and 7 from week 6

--- modulo_II/Semana 6/Exercícios da Semana/exercicios-semana-6/src/App.jsx
+++ modulo_II/Semana 6/Exercícios da Semana/exercicios-semana-6/src/App.jsx
@@ -1,14 +1,16 @@
 import Header from "./components/Header/Header";
 import Banner from "./components/Banner/Banner";
 import Main from "./components/Main/Main";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <>
+    <div className="app-container h-100">
     <Header></Header>
     <Banner slogan={'Sempre prontos para lhe ajudar na sua aventura!'}></Banner>
     <Main></Main>
-    </>
+    <Footer></Footer>
+    </div>
   );
 }
 
--- modulo_II/Semana 6/Exercícios da Semana/exercicios-semana-6/src/components/ProductCard/ProductCard.jsx
+++ modulo_II/Semana 6/Exercícios da Semana/exercicios-semana-6/src/components/ProductCard/ProductCard.jsx
@@ -1,19 +1,22 @@
 import PropTypes from "prop-types";
-import "./ProductCard.css"
+import "./ProductCard.css";
 
 function ProductCard({ name, price, description, features, imageUrl, id }) {
   return (
     <div className="card" key={id}>
-      <img src={imageUrl} className="card-img-top"  alt="..." />
+      <img src={imageUrl} className="card-img-top" alt="..." />
       <div className="card-body">
         <h5 className="card-title">{name}</h5>
         <p className="card-text">{description}</p>
         <ul className="card-text">
-            {features.map((f, i) => {
-                <li key={i}>{f}</li>                
-            }) }
+          {features.map((f, i) => (
+            <li key={i}>{f}</li>
+          ))}
         </ul>
-        <p className="card-text"><small className="text-body-secondary">R$: {price}</small></p>
+
+        <p className="card-text">
+          <small className="text-body-secondary">R$: {price}</small>
+        </p>
       </div>
     </div>
   );
